46772cdb5aeeb3d20c738633f55501e29cabacb6
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-02-23 18:21:28 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let jokes = {};
+
+$(async function () {
+    for (let i = 0; i < 10; i++) {
+        const joke = await axios.get('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'text/plain'
+            }
+        });
+        jokes[joke.data] = 0;
+        $('#joke_container').append(createHtmlJoke(joke.data, 0));
+    }
+});
+
+function createHtmlJoke(joke, score) {
+    return `
+    <div id="joke_div">
+    <p class="joke">${joke}</p>
+    <p class="net-count">${score}</p>
+    <button id="up">Up</button>
+    <button id="down">Down</button>
+    </div>`
+}
+
+$('#joke_container').on('click', '#up', (e) => {
+    let joke = $(e.target).parent().find('.joke').text();
+    jokes[joke]++;
+    $(e.target).parent().find('.net-count').text(jokes[joke])
+    reOrderJokes();
+})
+
+$('#joke_container').on('click', '#down', (e) => {
+    let joke = $(e.target).parent().find('.joke').text();
+    jokes[joke]--;
+    $(e.target).parent().find('.net-count').text(jokes[joke])
+    reOrderJokes();
+})
+
+function reOrderJokes() {
+    $('#joke_container').empty();
+    let sorted = Object.entries(jokes).sort((a, b) => b[1] - a[1]);
+    for (let [joke, score] of sorted) {
+        $('#joke_container').append(createHtmlJoke(joke, score));
+    }
+}
+
